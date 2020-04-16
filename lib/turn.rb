1f6b1423ee5f31d386fa1f4dfa06aabce855610a
Erik Weir <erik.weir@key.me>
2020-04-16 21:21:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,4 +46,7 @@ def turn(board)
   puts "Please enter 1-9:"
   num = gets.chomp
   index = input_to_index(num)
-end
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  end
